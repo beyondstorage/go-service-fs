e8a5daa9783a4908e142765bcb6d81f3a6043480
Xuanwo <github@xuanwo.io>
2020-11-10 16:12:09 +0800

    *: Fix go-storage definitions change (#22)
    * *: Fix go-storage definitions change
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Updaet go mod files
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- doc.go
+++ doc.go
@@ -3,4 +3,4 @@ Package fs provided support for local file system.
 */
 package fs
 
-//go:generate definitions service.hcl
+//go:generate go run github.com/aos-dev/go-storage/v2/cmd/definitions service.hcl
