34ce8215bca9ed62a8e8edab3da91ec88198cfc8
Xuanwo <github@xuanwo.io>
2021-04-22 15:28:19 +0800

    tests: Add check for flag
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/storage_test.go
+++ tests/storage_test.go
@@ -1,15 +1,22 @@
 package tests
 
 import (
+	"os"
 	"testing"
 
 	tests "github.com/aos-dev/go-integration-test/v3"
 )
 
 func TestStorage(t *testing.T) {
+	if os.Getenv("STORAGE_FS_INTEGRATION_TEST") != "on" {
+		t.Skipf("STORAGE_FS_INTEGRATION_TEST is not 'on', skipped")
+	}
 	tests.TestStorager(t, setupTest(t))
 }
 
 func TestAppend(t *testing.T) {
+	if os.Getenv("STORAGE_FS_INTEGRATION_TEST") != "on" {
+		t.Skipf("STORAGE_FS_INTEGRATION_TEST is not 'on', skipped")
+	}
 	tests.TestAppender(t, setupTest(t))
 }
