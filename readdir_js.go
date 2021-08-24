39e5b5ac93c69553e8970fea4fb5e195664ad667
Xuanwo <github@xuanwo.io>
2021-08-24 17:21:59 +0800

    feat: Add cross build support (#72)
    * feat: Add cross build support
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Run build only
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ readdir_js.go
@@ -0,0 +1,11 @@
+package fs
+
+import (
+	"context"
+
+	typ "github.com/beyondstorage/go-storage/v4/types"
+)
+
+func (s *Storage) listDirNext(ctx context.Context, page *typ.ObjectPage) (err error) {
+	panic("not implemented")
+}
